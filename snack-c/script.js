8d4bc24ecb51a8ea9e3ccd357dec00e88d6f506d
TommasoBernardo <tommasob1210@gmail.com>
2022-12-06 11:42:30 +0100

    finish exercise

---
+++ snack-c/script.js
@@ -0,0 +1,29 @@
+// Crea un array di oggetti che rappresentano degli animali.
+// Ogni animale ha un nome, una famiglia e una classe.
+// Es:
+
+// [
+//   { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+//   { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+//   { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+// ]
+
+// Crea un nuovo array con la lista dei mammiferi.
+// Bonus 1 : trasforma ogni valore di ogni animale in tutto maiuscolo tranne la prima lettera, tipo // leone => lEONE, canidi => cANIDI
+
+
+const animals = [
+    {nome : 'leone',  famiglia: 'felidi', classe: 'mammiferi'},
+
+    {nome:'cane', famiglia: 'canidi', classe: 'mammiferi'},
+
+    {nome:'gatto', famiglia: 'gattidi', classe:'felini'},
+
+    {nome:'tigre', famiglia:'tigrotti', classe:'felini'}, 
+
+    {nome:'elefante', famiglia:'elefanti',classe:'mammiferi'},
+]
+
+const mammiferi = animals.filter(() => animals.classe === 'mammiferi');
+
+console.log(mammiferi)
